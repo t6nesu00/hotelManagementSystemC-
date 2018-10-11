02f620a959b26277af13a4775eef3d9eed84ffe1
Suman Nepali <hlw.zigrigmail.com>
2018-10-12 01:08:21 +0300

    Login and Logout

---
+++ MyApplication/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(Session["user"]== null)
+                {
+                return RedirectToAction("Login", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ MyApplication/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApplication.Models;
+
+namespace MyApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            if("admin".Equals(name) && "123".Equals(password))
+            {
+                Session["user"] = new User() { Login = name, Name = "Tsuman" };
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            //or Session["user"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
